c32eec8f8a6baa26622ab2e3f1c6be05d07f0d66
ayushchaurasiya289 <147719677+AyushChaursiaya@users.noreply.github.com>
2026-04-30 15:19:52 +0530

    task

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,11 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://assessment-task-bice.vercel.app",
+    ],
+
     credentials: true,
   }),
 );
